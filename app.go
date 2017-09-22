ee6c76d90cf3d143f9c0435d920ed958253e8e77
diman94 <94demon@gmail.com>
2017-09-22 11:33:07 +0300

    some error handling

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 "io/ioutil"
 "encoding/json"
 "time"
+"errors"
 )
 
 const api_url string = "https://bittrex.com/api/v1.1/public/getticker?market="
@@ -22,28 +23,46 @@ type ResponseResultType struct {
 	Last   float64
 }
 
-func CheckCurrency(currency string, m map[string]float64) {
-	resp, err := http.Get(api_url+currency)
+func CheckCurrency(currency *string, m map[string]float64) (err error) {
+	resp, err := http.Get(api_url+*currency)
 	if err != nil {
-		// handle error
+		err := errors.New("could not fetch data")
+		fmt.Println(err)
+		return err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		// handle error
+		err := errors.New("could not fetch data")
+		fmt.Println(err)
+		return err
 	}
 	// parse json to struct
 	var response ApiResponse
 	json.Unmarshal(body, &response)
-    // compare with old value
-	if response.Result.Last != m[currency]{
-		fmt.Print(time.Now().Format("15:04:05.99 "))
-		fmt.Print(currency)
-		fmt.Printf(" Last = %.8f", response.Result.Last)
-		fmt.Printf(" was = %.8f\n", m[currency])
-		// assign new value
-		m[currency] = response.Result.Last
+	if response.Success != true {
+		err := errors.New("API error: " + response.Message + " market: " + *currency)
+		fmt.Println(err)
+		return err
 	}
+    // compare with old value
+	if response.Result.Last != m[*currency]{
+		fmt.Print(time.Now().Format("15:04:05.99 "))
+		fmt.Print(*currency)
+		fmt.Printf(" Last = %.8f ", response.Result.Last)
+		// print some arrows
+		switch {
+		case m[*currency] == 0: // first iteration
+			fmt.Println()
+		case response.Result.Last < m[*currency]:
+			fmt.Println("↓")
+		default:
+			fmt.Println("↑")
+		}
+		// assign new value
+		m[*currency] = response.Result.Last
+	}
+	return nil
 }
 
 func main() {
@@ -58,7 +77,7 @@ func main() {
 		if i == len(args) {
 			i = 0
 		}
-		go CheckCurrency(args[i], m) // no time shifts here
+		go CheckCurrency(&args[i], m) // no time shifts here
 		time.Sleep(delay)
 	}
 }
